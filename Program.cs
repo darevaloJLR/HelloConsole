b208155a51fadaf275befb6a4048f41634bc5e95
darevalo <darevalo@jaguarlandrover.com>
2020-03-10 11:48:27 +0000

    Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+        }
+
+        public static String createMessage()
+        {
+            return "Hello Console";
+        }
+    }
+}
