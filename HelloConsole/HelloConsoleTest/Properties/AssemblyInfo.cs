07037f46facc6a9a4fd819dca2aa656b97371cd9
darevalo <darevalo@jaguarlandrover.com>
2020-03-10 11:51:28 +0000

    First commit

---
+++ HelloConsole/HelloConsoleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloConsoleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jaguar Landrover")]
+[assembly: AssemblyProduct("HelloConsoleTest")]
+[assembly: AssemblyCopyright("Copyright © Jaguar Landrover 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f664197b-c589-41bb-abce-440917642e22")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloConsole/HelloConsoleTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloConsole;
+
+namespace HelloConsoleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello Console", Program.createMessage());
+        }
+    }
+}
